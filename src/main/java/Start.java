8488bae4f9ad203747696fac100de9113152c60d
MatteoElia06 <s8901798d@majorana-giorgi.edu.it>
2025-02-03 09:44:08 +0100

    #2 commit di analisi requisito

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,7 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ciao");
-
+		System.out.println("ciao2");
+		System.out.println("ciao3");
 	}
 
 }
